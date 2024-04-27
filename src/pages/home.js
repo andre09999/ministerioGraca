64d2bd162ee10801f81ce4e9a1b7c9a8b147461b
Erika Ribeiro <122754392+erika3724@users.noreply.github.com>
2024-04-27 15:32:06 -0300

    atualizando

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,28 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './home.css'
 import Pc from '../Images/pc.mp4'
 import celular from '../Images/celular.mp4'
+
 function Home() {
   const [video, setVideo] = useState('homepc')
   const [video1 , setVideo1] = useState('esconde')
+
   useEffect(() => {
-    if (window.innerWidth < 600) {
-      setVideo('esconde')
-      setVideo1('homecell')
-  } else {
-    setVideo1('esconde')
-    setVideo('homepc')
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window.innerWidth]);
-  
+    function handleResize() {
+      if (window.innerWidth < 600) {
+        setVideo('esconde')
+        setVideo1('homecell')
+      } else {
+        setVideo1('esconde')
+        setVideo('homepc')
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); 
+
   return (
     <div className="home">
       <video className={video} src={Pc} autoPlay loop muted />
