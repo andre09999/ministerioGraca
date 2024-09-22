f18ded71baeea27383f73207df7db309ac5259d4
Andre Luis Ribeiro de Souza <andre09999@live.com>
2024-09-21 21:00:29 -0300

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,78 +1,71 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Instagram from '../Images/instagram.png'
-import Facebook from '../Images/facebook.png'
-import Youtube from '../Images/youtube.png'
-import whatsapp from '../Images/whatsapp.png'
-import maps from '../Images/maps.png'
-import './footer.css'
+import Instagram from '../Images/instagram.png';
+import Facebook from '../Images/facebook.png';
+import Youtube from '../Images/youtube.png';
+import whatsapp from '../Images/whatsapp.png';
+import maps from '../Images/maps.png';
+import './footer.css';
+
 function Footer() {
   const location = useLocation();
-  const [video, setVideo] = useState('footerpc')
-  const [video1, setVideo1] = useState('esconde')
-  const [fot , setFot] = useState('content')
-  const routeChange = (e) =>{ 
-    if(e === 'Youtube'){
-      window.open('https://www.youtube.com/@ministeriogracago','_blank')
-    }
-    if(e === 'Instagram'){
-      window.open('https://www.instagram.com/ministeriogracago/','_blank')
-    }
-    if(e === 'Facebook'){
-      window.open('https://www.facebook.com/profile.php?id=61557652214404','_blank')
-    }
-    if(e === 'whatsapp'){
-      window.open('https://wa.me/5562996226821?text=Gra%C3%A7a+e+paz','_blank')
-    }
-    if(e === 'maps'){
-      window.open('https://maps.app.goo.gl/RbtqRh87TPhVC1F5A','_blank')
-    }
-  }
+  const [video, setVideo] = useState('footerpc');
+  const [video1, setVideo1] = useState('esconde');
+  const [fot, setFot] = useState('content');
+
+  const routeChange = (e) => {
+    const urls = {
+      'Youtube': 'https://www.youtube.com/@ministeriogracago',
+      'Instagram': 'https://www.instagram.com/ministeriogracago/',
+      'Facebook': 'https://www.facebook.com/profile.php?id=61557652214404',
+      'whatsapp': 'https://wa.me/5562996226821?text=Gra%C3%A7a+e+paz',
+      'maps': 'https://maps.app.goo.gl/RbtqRh87TPhVC1F5A',
+    };
+    window.open(urls[e], '_blank');
+  };
 
   useEffect(() => {
-    if (window.innerWidth < 600) {
-      setVideo('esconde')
-      setVideo1('footercell')
-  } else {
-    setVideo1('esconde')
-    setVideo('footerpc')
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window.innerWidth]);
-  
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setVideo('esconde');
+        setVideo1('footercell');
+      } else {
+        setVideo1('esconde');
+        setVideo('footerpc');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   useEffect(() => {
-    console.log(location.pathname === '/')
-    if (location.pathname === '/') {
-      setFot('content')
-    }
-    if (location.pathname === '/sobre') {
-      setFot('content')
-    }
-    if (location.pathname === '/servir') {
-      setFot('content')
-    }
-    if (location.pathname === '/kids') {
-      setFot('esconde')
-    }
+    const isKidsPage = location.pathname === '/kids';
+    setFot(isKidsPage ? 'esconde' : 'content');
   }, [location.pathname]);
 
   return (
-    <div className={`${fot}`}>
-    <footer className={video}>
-        <button onClick={() => routeChange('Youtube')}> <img src={Youtube} alt="Youtube"/> </button>
-        <button onClick={() => routeChange('Instagram')}> <img src={Instagram} alt="instagram"/> </button>
-        <button onClick={() => routeChange('Facebook')}> <img className="facebook" src={Facebook}
-          alt="Facebook" /> </button>
-        <button onClick={() => routeChange('maps')}> <img src={maps} alt="maps" /> </button>
-        <button onClick={() => routeChange('whatsapp')}> <img src={whatsapp} alt="whatsapp" /> </button>
-      </footer>
-      <footer className={video1}>
-        <button onClick={() => routeChange('Youtube')}> <img src={Youtube} alt="Youtube" /> </button>
-        <button onClick={() => routeChange('whatsapp')}> <img src={whatsapp} alt="whatsapp" /> </button>
-        <button onClick={() => routeChange('maps')}> <img src={maps} alt="maps"/> </button>
-        <button onClick={() => routeChange('Facebook')}> <img className="facebook" src={Facebook} alt="Facebook"/> </button>
-        <button onClick={() => routeChange('Instagram')}> <img src={Instagram} alt="instagram"/> </button>
-    </footer>
+    <div className="container1">
+      <div className={`${fot}`}>
+        <footer className={video}>
+          <button onClick={() => routeChange('Youtube')}> <img src={Youtube} alt="Youtube" /> </button>
+          <button onClick={() => routeChange('Instagram')}> <img src={Instagram} alt="Instagram" /> </button>
+          <button onClick={() => routeChange('Facebook')}> <img className="facebook" src={Facebook} alt="Facebook" /> </button>
+          <button onClick={() => routeChange('maps')}> <img src={maps} alt="Maps" /> </button>
+          <button onClick={() => routeChange('whatsapp')}> <img src={whatsapp} alt="WhatsApp" /> </button>
+        </footer>
+        <footer className={video1}>
+          <button onClick={() => routeChange('Youtube')}> <img src={Youtube} alt="Youtube" /> </button>
+          <button onClick={() => routeChange('whatsapp')}> <img src={whatsapp} alt="WhatsApp" /> </button>
+          <button onClick={() => routeChange('maps')}> <img src={maps} alt="Maps" /> </button>
+          <button onClick={() => routeChange('Facebook')}> <img className="facebook" src={Facebook} alt="Facebook" /> </button>
+          <button onClick={() => routeChange('Instagram')}> <img src={Instagram} alt="Instagram" /> </button>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,30 @@
 import React from "react";
-import './about.css'
+import "./about.css";
+import voluntarios from '../Images/voluntarios.jpg'
 
 function About() {
   return (
     <main className="about">
-    <h1>Sobre o Ministério Graça</h1>
-    <section>
-    <p>"Amar a Deus e servir pessoas.</p>
-    <p>Vem pra Graça você também, Vem!"</p>
-    <p>(Pastor Leonardo Fagundes)</p>
-    </section>
-    <section>
-      
-    </section>
+      <div className="overlay">
+        <h1 className="colorabout">Descubra mais sobre o Ministério Graça</h1>
+
+        <p className="colorabout">
+          O Ministério Graça surgiu a partir de uma chamada divina, nos
+          convidando a adotar um modelo de voluntariado.
+        </p>
+        <p className="colorabout">
+          Nossa missão é alcançar toda a família, e para isso, organizamos
+          nossas atividades focando em crianças, adolescentes, jovens, adultos e
+          idosos, reconhecendo a importância de cuidar e formar cada fase do
+          desenvolvimento humano à luz de Cristo. Como parte do processo de
+          resgatar indivíduos do reino das trevas para o Reino da Luz, o
+          Ministério Graça tem se fortalecido no evangelismo profético, que
+          abrange: salvação, libertação, cura, voluntariado e restauração do ser
+          humano. Com esse mesmo propósito, também realizamos obras sociais,
+          como o projeto "Graça nas Ruas".
+        </p>
+      </div>
+      <img alt="voluntarios" className="vol" src={voluntarios} />
     </main>
   );
 }
